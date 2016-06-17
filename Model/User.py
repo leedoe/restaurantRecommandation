c1a58200e967cdd8593aa12891b9b2c1b86b3949
hawai <hawaida@naver.com>
2016-06-18 00:02:04 +0900

    User class

--- Model/User.py
+++ Model/User.py
@@ -1,19 +1,83 @@
-#-*- encoding=utf-8 -*-
+from Model.FoodPreference  import FoodPreference
+from Model.Food  import Food
 
 class User:
 
-    def __init__(self, email, password, age, gender):
-        self._email = email
-        self._password = password
-        self._age = age
-        self._gender = gender
-        self._foodPreferences = []
+    def __init__(self, email, password, age, gender, foodPreference):
+        self.email = email
+        self.password = password
+        self.age = age
+        self.gender = gender
+        self.foodPreference = []
 
-    def addPreference(self):
-        pass
+    def addPreference(self, foodName, score):
+        food = Food(foodName)
+        foodPreference = FoodPreference(score, food)
+        self.foodPreference.append(foodPreference)
 
-    def modifyPreference(self):
-        pass
+    def modifyPreference(self, foodName, score):
+        for foodPreference in self.foodPreference:
+            food = foodPreference.getFood()
+            if food.getName() == foodName:
+                food.setScore(score)
+                break
 
-    def deletePreference(self):
-        pass
+
+    def deletePreference(self, foodName):
+        for foodPreference in self.foodPreference:
+            food = foodPreference.getFood()
+            if food.getName() == foodName:
+                self.foodPreference.remove(foodPreference)
+                break
+
+    @property
+    def getFoodPreference(self):
+        return self.foodPreference
+
+    @property
+    def getUserId(self):
+        return self.userId
+
+    @property
+    def getEmail(self):
+        return self.email
+
+    @property
+    def getPassword(self):
+        return self.password
+
+    @property
+    def getAge(self):
+        return self.age
+
+    @property
+    def getGender(self):
+        return self.gender
+
+    @property
+    def getFoodPreference(self):
+        return self.foodPreference
+
+    @userId.setter
+    def setUserId(self,userId):
+        self.userId = userId
+
+    @email.setter
+    def setEmail(self,email):
+        self.email = email
+
+    @password.setter
+    def setPassword(self,password):
+        self.password = password
+
+    @age.setter
+    def setAge(self,age):
+        self.age = age
+
+    @gender.setter
+    def setGender(self,gender):
+        self.gender = gender
+
+    @foodPreference.setter
+    def setFoodPreference(self,foodPreference):
+        self.foodPreference = foodPreference
