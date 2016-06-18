31827f6fde234e9030eb848b1dd1261c8ec9d4ba
hawai <hawaida@naver.com>
2016-06-19 00:36:58 +0900

    FoodPreference class

--- Model/FoodPreference.py
+++ Model/FoodPreference.py
@@ -1,11 +1,35 @@
 #-*- encoding=utf-8 -*-
 
+
 class FoodPreference:
 
-    def __init__(self):
-        self._foodPreferenceID = int
-        self._food = str()
-        self._score = int
+    def __init__(self, foodName, score):
+        self._foodName = foodName
+        self._score = score
 
-    def getScore(self):
+
+    @property
+    def foodName(self):
+        return self._foodName
+
+    @foodName.setter
+    def foodName(self,value):
+        self._foodName = value
+
+    @foodName.deleter
+    def foodName(self):
+        del self._foodName
+
+
+    @property
+    def score(self):
         return self._score
+
+    @score.setter
+    def score(self,value):
+        self._score = value
+
+    @score.deleter
+    def score(self):
+        del self._score
+
