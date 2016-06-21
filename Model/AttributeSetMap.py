351238710e42822929bec70b3d390c105c94d85e
hawai <hawaida@naver.com>
2016-06-21 16:43:02 +0900

    AttributeSetMap class(미 검수)

---
+++ Model/AttributeSetMap.py
@@ -0,0 +1,52 @@
+#-*- encoding=utf-8 -*-
+
+class AttributeSetMap:
+
+    def __init__(self):
+        self._map = {}
+
+    def addAttributeSet(self, attributeSetCode, attributeSet):
+        map[attributeSetCode] = attributeSet
+
+    def addElementInAttributeSet(self, attributeSetCode, element):
+        map[attributeSetCode].add(element)
+
+    def searchAttributeSet(self, attributeSetCode):
+        return map[attributeSetCode]
+
+    def searchElementInAttributeSet(self, attributeSetCode, element):
+        list = list(map[attributeSetCode])
+        for i in list:
+            if element == i:
+                return True
+        return False
+
+    def modifyAttributeSetCode(self, oldAttributeSetCode, newAttributeSetCode):
+        map[newAttributeSetCode] = map[oldAttributeSetCode]
+        del map[oldAttributeSetCode]
+
+    def modifyElementInAttributeSet(self, attributeSetCode, newElement):
+        map[attributeSetCode] = newElement
+
+    def deleteAttributeSet(self, attributeSetCode):
+        del map[attributeSetCode]
+
+    def deleteElementInAttributeSet(self, attributeSetCode, element):
+        list = list(map[attributeSetCode])
+        for i in list:
+            if element == i:
+                del list[i]
+                break
+
+    @property
+    def map(self):
+        return self._map
+
+    @map.setter
+    def map(self,value):
+        self._map = value
+
+    @map.deleter
+    def map(self):
+        del self._map
+
