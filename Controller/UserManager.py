ea54d68262c8a56b41ce5034f476a1fc44869b9b
hawai <hawaida@naver.com>
2016-06-21 16:34:04 +0900

    UserManager class(미 검수_ UserDBManager에 포함 될수 있음)

--- Controller/UserManager.py
+++ Controller/UserManager.py
@@ -1,25 +1,43 @@
 # -*- encoding=utf-8 -*-
 
 from Model.Singleton import Singleton
+from Model.User import User
+from Controller.UserDBManager import UserDBManger
 
+#유저 객체를 관리하는 매니저 클래스
 class UserManager:
     __metaclass__ = Singleton
 
     def __init__(self):
-        self._userDBManager
-        pass
+        self._userDBManager = UserDBManger()
 
-    def searchUserByEmail(self):
-        pass
+    def searchUserByEmail(self,email):
+        self._userDBManger.searchUser()
 
-    def makeUser(self):
-        pass
+    #유저 객체를 생성한다음에 db에 저장
+    def makeUser(self,email,password,age,gender):
+        self._user = User(email, password, age, gender)
+        self._userDBManger.insertUser(self._user)
 
-    def modifyUser(self):
-        pass
+    def modifyUser(self,email,password):
+        self._userDBManger.modifyUser()
 
-    def deleteUser(self):
-        pass
+    def deleteUser(self,email,password):
+        self._userDBManger.deleteUser()
 
-    def getUserPreferences(self):
-        pass
+    def getUserPreferences(self,user):
+        user = self._userDBManger.searchUser()
+        return user.foodPreference
+
+    @property
+    def userDBManger(self):
+        return self._userDBManger
+
+    @userDBManger.setter
+    def userDBManger(self,value):
+        self._userDBManger = value
+
+    @userDBManger.deleter
+    def userDBManger(self):
+        del self._userDBManger
+
