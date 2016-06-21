3754bd7082cb7efccb2cca03c1c00d4ac54a6737
hawai <hawaida@naver.com>
2016-06-21 17:11:59 +0900

    AttributeSet class

--- Model/AttributeSet.py
+++ Model/AttributeSet.py
@@ -2,18 +2,20 @@
 
 from Model.Attribute import Attribute
 
+#음식속성에 대한 정보를 집합으로 관리하는 클래스
 class AttributeSet:
-
+    # 멤버변수:집합id,음식 속성 집합
     def __init__(self,attributeSetID):
         self._attributeSetID = attributeSetID
-        self._attributeSet = ()
+        self._attributeSet = set()
 
-
+    #속성 집합에 속성을 추가
     def addAttribute(self,attribute):
-        self.attributeSet.add(attribute)
+        self._attributeSet.add(attribute)
 
+    #속성 집합에 속성을 제거
     def removeAttribute(self,attribute):
-        self.attributeSet.remove(attribute)
+        self._attributeSet.remove(attribute)
 
 
     @property
