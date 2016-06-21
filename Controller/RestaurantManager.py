dc3c7742e6d405086e702afd0b41a7ab92f0fe41
hawai <hawaida@naver.com>
2016-06-21 16:26:36 +0900

    RestaurantManager class(미 검수)

--- Controller/RestaurantManager.py
+++ Controller/RestaurantManager.py
@@ -1,23 +1,68 @@
 # -*- encoding=utf-8 -*-
 
 from Model.Singleton import Singleton
+from Model.Restaurant import Restaurant
+from Model.RestaurantList import RestaurantList
+from Controller.RestaurantDBManager import RestaurantDBManager
 
+#식당 객체를 관리하는 매니저 클래스
 class RestaurantManager:
     __metaclass__ = Singleton
 
     def __init__(self):
         self._restaurantList = []
-        self._restaurantDBManager
-        pass
+        self._restaurantDBManager = RestaurantDBManager()
 
-    def addRestaurant(self):
-        pass
+    def addRestaurant(self,name,location,foods):
+        restaurant = Restaurant(name, location, foods)
+        self.restaurantList.restaurantList.append(restaurant)
 
-    def modifyRestaurant(self):
-        pass
+    def modifyRestaurantByName(self, name, newName):
+        for restaurant in self.restaurantList.restaurantList:
+            if (restaurant.getName() == name):
+                restaurant.setName(newName)
+                break
 
-    def deleteRestaurant(self):
-        pass
+    def modifyRestaurantByLocation(self,name,location):
+        for restaurant in self.restaurantList.restaurantList:
+            if(restaurant.getName() == name):
+                restaurant.setLocation(location)
+                break
 
-    def getRestaurantByLocationAndFood(self):
-        pass
+    def modifyRestaurantByFoods(self, name, foods):
+        for restaurant in self.restaurantList.restaurantList:
+            if (restaurant.getName() == name):
+                restaurant.setFoods(foods)
+                break
+
+    def deleteRestaurantByName(self,name):
+        for restaurant in self.restaurantList.restaurantList:
+            if (restaurant.getName() == name):
+                self.restaurantList.restaurantList.remove(restaurant)
+                break
+
+
+    @property
+    def restaurantList(self):
+        return self._restaurantList
+
+    @restaurantList.setter
+    def restaurantList(self, value):
+        self._restaurantList = value
+
+    @restaurantList.deleter
+    def restaurantList(self):
+        del self._restaurantList
+
+
+    @property
+    def restaurantDBManager(self):
+        return self._restaurantDBManager
+
+    @restaurantDBManager.setter
+    def restaurantDBManager(self, value):
+        self._restaurantDBManager = value
+
+    @restaurantDBManager.deleter
+    def restaurantDBManager(self):
+        del self._restaurantDBManager
