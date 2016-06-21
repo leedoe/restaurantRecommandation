5899485de0078d34df9a2634165ac12c5cfa1bc7
hawai <hawaida@naver.com>
2016-06-21 16:08:47 +0900

    FoodManager class (미검수)

--- Controller/FoodManager.py
+++ Controller/FoodManager.py
@@ -1,29 +1,73 @@
 # -*- encoding=utf-8 -*-
 
 from Model.Singleton import Singleton
+from Model.Food import Food
+from Model.FoodList import FoodList
+from Controller.FoodDBManager import FoodDBManager
 
+#음식 객체를 관리하는 매니저 클래스
 class FoodManager:
     __metaclass__ = Singleton
 
     def __init__(self):
         self.foodList = []
         self.foodDBManager
-        pass
 
-    def makeFood(self):
-        pass
+    def makeFood(self, name, attributeSetMap):
+        food = Food(name, attributeSetMap)
+        self.foodList.foodList.append(food)
 
-    def searchFood(self):
-        pass
+    def searchFood(self, foodName):
+        for food in self.foodList.foodList:
+            if food.getName() == foodName:
+                return food
 
-    def deleteFood(self):
-        pass
+    def deleteFood(self, foodName):
+        for food in self.foodList.foodList:
+            if food.getName() == foodName:
+                self.foodList.foodList.remove(food)
 
-    def compareFoodAttribute(self):
-        pass
+    def compareFoodAttribute(self, foodNameA, foodNameB, attributeSetCodeA, attributeSetCodeB):
+        attributeSetMapA = foodNameA.getAttributeSetMap()
+        attributeSetMapB = foodNameB.getAttributeSetMap()
+        if (attributeSetMapA[attributeSetCodeA] == attributeSetMapB[attributeSetCodeB]):
+            return True
+        return False
 
-    def intersectionFoodAttribute(self):
-        pass
+    def intersectionFoodAttribute(self, foodNameA, foodNameB, attributeSetCodeA, attributeSetCodeB):  # 교집합 찾기
+        for food in self.foodList.foodList:
+            if food.name == foodNameA:
+                attributeSetMapA = food.attributeSetMap
+            if food.name == foodNameB:
+                attributeSetMapB = food.attributeSetMap
+        return attributeSetMapA[attributeSetCodeA].intersection(attributeSetMapB[attributeSetCodeB])
 
-    def unionFoodAttribute(self):
-        pass
+    def intersectionFoodAttribute(self, foodA, foodB, attributeSetCodeA, attributeSetCodeB):
+        attributeSetMapA = foodA.attributeSetMap
+        attributeSetMapB = foodB.attributeSetMap
+        return attributeSetMapA[attributeSetCodeA].intersection(attributeSetMapB[attributeSetCodeB])
+
+    def unionFoodAttribute(self, foodNameA, foodNameB, attributeSetCodeA, attributeSetCodeB):  # 합집합 찾기
+        for food in self.foodList.foodList:
+            if food.name == foodNameA:
+                attributeSetMapA = food.attributeSetMap
+            if food.name == foodNameB:
+                attributeSetMapB = food.attributeSetMap
+        return attributeSetMapA[attributeSetCodeA].union(attributeSetMapB[attributeSetCodeB])
+
+    def unionFoodAttribute(self, foodA, foodB, attributeSetCodeA, attributeSetCodeB):
+        attributeSetMapA = foodA.attributeSetMap
+        attributeSetMapB = foodB.attributeSetMap
+        return attributeSetMapA[attributeSetCodeA].union(attributeSetMapB[attributeSetCodeB])
+
+    @property
+    def foodList(self):
+        return self._foodList
+
+    @foodList.setter
+    def foodList(self, foodList):
+        self._foodList = foodList
+
+    @foodList.deleter
+    def foodList(self):
+        del self._foodList
