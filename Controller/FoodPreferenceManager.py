d7e24251bd2d378c8405473d1228ff21091ee149
hawai <hawaida@naver.com>
2016-06-21 16:31:19 +0900

    RestaurantManager class(미 검수)

--- Controller/FoodPreferenceManager.py
+++ Controller/FoodPreferenceManager.py
@@ -1,15 +1,30 @@
 # -*- encoding=utf-8 -*-
 
 from Model.Singleton import Singleton
+from Controller.FoodPreferenceDBManager import FoodPreferenceDBManager
 
+#음식선호도 객체를 관리하는 매니저 클래스
 class FoodPreferenceManager:
     __metaclass__ = Singleton
 
     def __init__(self):
-        self.foodPreferenceDBManager
+        self._foodPreferenceDBManger=FoodPreferenceDBManager()
 
-    def getUsersByFoodName(self):
-        pass
+    def getUsersByFoodName(self,foodName):
+        self.foodPreferenceDBManger.searchUsersByFoodName(foodName)
 
-    def getFoodsByUserID(self):
-        pass
+    def getFoodsByUserID(self,id):
+        self.foodPreferenceDBManger.searchFoodsByUserID(id)
+
+
+    @property
+    def foodPreferenceDBManger(self):
+        return self._foodPreferenceDBManger
+
+    @foodPreferenceDBManger.setter
+    def foodPreferenceDBManger(self,value):
+        self._foodPreferenceDBManger =value
+
+    @foodPreferenceDBManger.deleter
+    def foodPreferenceDBManger(self, value):
+        del self._foodPreferenceDBManger
