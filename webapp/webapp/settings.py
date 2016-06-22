435c9227fea47f1125a5aa2cb60b7d66b3247b27
이도준 <leedoe@naver.com>
2016-06-22 20:10:35 +0900

    DB setting

--- webapp/webapp/settings.py
+++ webapp/webapp/settings.py
@@ -85,6 +85,8 @@ DATABASES = {
         'NAME': 'django',
         'USER': '',
         'PASSWORD': '',
+        'HOST': 'project.czpuraarclth.ap-northeast-2.rds.amazonaws.com',
+        'PORT': '3306'
     }
 }
 
