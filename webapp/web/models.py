3b71d93488a81503cbdc1ddcf21839286324d8dc
이도준 <leedoe@naver.com>
2016-06-22 10:35:11 +0900

    write django models UserInfo, Food, FoodAttribute, Preference, Restaurant, RestaurantScore(Perference)

--- webapp/web/models.py
+++ webapp/web/models.py
@@ -1,3 +1,61 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+
 
 # Create your models here.
+
+
+# UserInformation
+class UserInfo(AbstractUser):
+    age = models.IntegerField(null=False)
+    location = models.CharField()
+
+    def __str__(self):
+        return self.username
+
+
+# Food Information
+class Food(models.Model):
+    name = models.CharField(null=False)
+
+    def __str__(self):
+        return self.name
+
+
+# Food Attribute
+class FoodAttribute(models.Model):
+    foodID = models.ForeignKey(Food)
+    attributeName = models.CharField()
+    contents = models.CharField()
+
+    def __str__(self):
+        return str(self.foodID) + " " + self.attributeName
+
+
+# User Preference
+class UserFoodPreference(models.Model):
+    userID = models.ForeignKey(UserInfo)
+    foodID = models.ForeignKey(Food)
+    score = models.FloatField()
+
+    def __str__(self):
+        return str(self.userID) + " " + str(self.foodID)
+
+
+# Restaurant
+class Restaurant(models.Model):
+    name = models.CharField()
+    location = models.CharField()
+
+    def __str__(self):
+        return self.name
+
+
+# Restaurant Score by User
+class RestaurantScore(models.Model):
+    userID = models.ForeignKey(UserInfo)
+    restaurantID = models.ForeignKey(Restaurant)
+    score = models.FloatField()
+
+    def __str__(self):
+        return str(self.userID) + " " + str(self.restaurantID)
